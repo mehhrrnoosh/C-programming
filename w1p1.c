2aacb8e27d8d1fa46755f38c7ba22ca72e0e8bcb
Mehrnoosh Sarang <106999802+mehhrrnoosh@users.noreply.github.com>
2024-03-28 11:06:07 -0400

    Create w1p1.c

---
+++ w1p1.c
@@ -0,0 +1,30 @@
+//
+////*****************************************************************************
+//<assessment name example: Workshop - #1 (Part-1)>
+//Full Name  : Mehrnoosh Sarang
+//Student ID#: 128498227
+//Email      : msarang@seneca.ca
+//Section    : ZRA
+//Authenticity Declaration:
+//I declare this submission is the result of my own work and has not been
+//        shared with any other student or 3rd party content provider. This submitted
+//piece of work is entirely of my own creation.
+//*****************************************************************************
+
+#include <stdio.h>
+
+int main() {
+    printf("Workshop 1 Part-1\n"
+           "=================\n");
+    printf("\n");
+    printf("I'm displaying this using the 'printf' stdio\n");
+    printf("(standard input output) library function!\n\n");
+    printf("Dear teacher,\n\n");
+    printf("  I promise I will work hard from this day onward.\n"
+           "  I acknowledge that practice is extremely important,\n"
+           "  so I will do all workshops, quizzes, and assignments.\n\n");
+    printf("Sincerely,\n\n");
+    printf("Mehrnoosh Sarang\n");
+    printf("Student ID# 128498227\n");
+    return 0;
+}
